73f4fc25847bb9c52b259fb4131a5574398ffb12
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-02 16:26:17 -0700

    Tasks remaining 8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,17 +8,22 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a, operator, b = map(int, sys.argv[1:4])
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-    operations = {
-            '+': add,
-            '-': sub,
-            '*': mul,
-            '/': div
-            }
+    valid_operators = ['+', '-', '*', '/']
 
-    if operator in operations:
-        result = operations[operator](a, b)
+    if operator in valid_operators:
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            result = div(a, b)
+
         print("{} {} {} = {}".format(a, operator, b, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
