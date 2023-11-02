857ed7fe3d89555657d02340eab97da2ef548ea2
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-02 15:57:52 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4.pyc
+    import hidden_4
 
-    module = dir(hidden_4.pyc)
+    module = dir(hidden_4)
 
     for name in module:
         if not name.startswith('__'):
