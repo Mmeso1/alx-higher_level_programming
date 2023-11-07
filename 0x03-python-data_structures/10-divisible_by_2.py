d5900952b5a89bd5999505d61664bce5c32fba59
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-06 16:16:39 -0800

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new_list = []
+        for i in my_list:
+            new_list.append(True if i % 2 == 0 else False)
+        return new_list
+    else:
+      return None
