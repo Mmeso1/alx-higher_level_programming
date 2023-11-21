1ec8aa3fbcccbf812b9fa999b7e1e24ff17c87ad
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-21 13:50:05 -0800

    Instantiating the square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""This is the square class"""
+
+
+class Square:
+    """Square class that defines a square by: (based on 1-square.py)"""
+    def __init__(self, size=0):
+        """__init__
+        The init method is like a constructor, it initializes the class
+        with the size value of the square
+
+        Attributes:
+            size(int): the size of the square
+
+        Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
