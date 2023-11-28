3fb1505f902388d5078bef2480772a78709b58cb
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-28 13:11:22 -0800

    task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    return 'BestSchool' * magic_string.n
-magic_string.n += 1
+def magic_string(static={"count": 0}):
+    static["count"] += 1
+    return "BestSchool" + (", BestSchool" * (static["count"] - 1))
