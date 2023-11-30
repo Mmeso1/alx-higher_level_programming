df53d52e3f990a821f39ca4fd3827ad8a98c378e
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-29 18:23:26 -0800

    Task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,9 +12,17 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
 
-    for i, char in enumerate(text):
-        if char in ['.', '?', ':']:
-            print(char)
-            print()
-        elif char != ' ':
-            print(char, end='')
+    flag = 0
+    for char in text:
+        if flag == 0:
+            if char == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if char in ['.', '?', ':']:
+                print(char)
+                print()
+                flag = 0
+            else:
+                print(char, end="")
