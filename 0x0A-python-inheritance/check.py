f71fc605581723df0e431235d3a2a77a89f0ee2f
Mmeso1 <anisiubammesoma8@gmail.com>
2023-12-04 16:59:55 -0800

    task 11

--- 0x0A-python-inheritance/check.py
+++ 0x0A-python-inheritance/check.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-
-Square = __import__('10-square').Square
+Square = __import__('11-square').Square
 
 s = Square(13)
 
 print(s)
 print(s.area())
-
