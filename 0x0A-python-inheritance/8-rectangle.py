e0ae63f2de568b45ba8d3aa51cf639c27d4cc871
Mmeso1 <anisiubammesoma8@gmail.com>
2023-12-04 16:11:42 -0800

    T8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" Rectangle Module """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-""" Rectangle """
 
 
 class Rectangle(BaseGeometry):
@@ -9,6 +11,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """ Intialization with validation """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
