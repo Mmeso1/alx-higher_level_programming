9428d0b3b1a8f61184d72631f6928bb7ea1eb0a2
Mmeso1 <anisiubammesoma8@gmail.com>
2023-12-05 15:22:38 -0800

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Pascal's Triangle """
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        triangle = []
+        for i in range(num_rows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+        return triangle
