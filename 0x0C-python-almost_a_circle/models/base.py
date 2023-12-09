d456122db47c01193cc94efa397bcaab3b627e1f
Mmeso1 <anisiubammesoma8@gmail.com>
2023-12-09 09:50:42 -0800

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Base Class Module """
+
+
+class Base:
+    """ The base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base instance.
+
+        Args:
+            id (int): The identity of the instance.
+        """
+        if id is None:
+             Base.__nb_objects += 1
+             self.id = Base.__nb_objects
+        else:
+            self.id = id
