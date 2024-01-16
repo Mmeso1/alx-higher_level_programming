649ec068638906b537b4c472dfbf0806abbd8680
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-16 13:31:19 -0800

    To utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,3 @@
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE first_name MODIFY VARCHER(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
