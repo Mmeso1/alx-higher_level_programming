a53a7515df000664bb35d48d81e1e6e2a3aa50c8
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-17 23:32:25 -0800

    Task 17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,21 @@
---- List all tv shows not related to Dexter
-SELECT tv_genres.name FROM tv_genres
-WHERE tv_genres.id NOT IN (SELECT tv_genres.id FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
-WHERE tv_shows.title = 'Dexter')
-ORDER BY tv_genres.name;
+-- Lists all genres not linked to
+-- the show `Dexter`.
+
+SELECT DISTINCT tv_genres.name
+  FROM tv_shows
+       JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+
+       JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_shows
+         JOIN tv_show_genres
+         ON tv_shows.id = tv_show_genres.show_id
+
+         JOIN tv_genres
+         ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
