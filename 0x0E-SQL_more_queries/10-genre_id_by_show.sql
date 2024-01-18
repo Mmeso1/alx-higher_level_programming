8e50dec8facb4727b125e973b298ab862e3856c1
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-17 23:34:48 -0800

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
--- lists all shows from hbtn_0d_tvshows_rate by their rating
-
-
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings.show_id
-GROUP BY tv_shows.id
-ORDER BY rating DESC;
+-- Lists all shows contained in `hbtn_0d_tvshows`
+-- that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
