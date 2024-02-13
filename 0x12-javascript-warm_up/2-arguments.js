7c8ab1faa6e2fadc96fcef99deee26a15cf70450
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-13 01:06:09 +0100

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const { argv } = require('node:process')
 
-if (!argv[2]) {
+if (argv.length == 2) {
   console.log('No argument')
+} else if (argv.length == 3) {
+  console.log('Argument found')
 } else {
   console.log('Arguments found')
 }
