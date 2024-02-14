f450c50e00415e4743be4c3d05983f7e102f853c
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-02-14 03:42:31 +0100

    Update 4-rectangle.js -

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -5,32 +5,32 @@
 module.exports = class Rectangle {
   constructor(w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
-      this.width = w
-      this.height = h
+      this.width = w;
+      this.height = h;
     }
   }
 
   print() {
     for (let i = 0; i < this.height; i++) {
-      let j = 0
+      let j = 0;
       for (; j < this.width; j++) {
-        process.stdout.write('X')
+        process.stdout.write('X');
       }
 
       if (j == this.width) {
-        console.log('')
+        console.log('');
       }
     }
   }
 
   rotate() {
-    let temp = this.width
-    this.width = this.height
-    this.height = temp
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
   double() {
-    this.width = this.width * 2
-    this.height = this.height * 2
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
-}
+};
