1bca6cf0d12a3d4dd85712f2f585c7c9ff59db26
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-14 01:37:56 +0100

    Task 5 to 8

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// defines a square and inherits from Square of 5-square.js
+
+const OldSquare = require('./5-square')
+
+module.exports = class Square extends OldSquare {
+  constructor(size) {
+    super(size, size)
+  }
+
+  charPrint(c = 'X') {
+    super.print(c)
+  }
+}
