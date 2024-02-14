843c2166c4f1d105a296d80098e1ffe877db2af4
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-02-14 04:25:01 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-const _dict = require('./101-data.js').dict
+const _dict = require('./101-data.js').dict;
 
-const newDict = {}
+const newDict = {};
 
 for (const key in _dict) {
-  const occurrence = _dict[key]
+  const occurrence = _dict[key];
 
   if (newDict.hasOwnProperty(occurrence)) {
-    newDict[occurrence].push(key)
+    newDict[occurrence].push(key);
   } else {
-    newDict[occurrence] = [key]
+    newDict[occurrence] = [key];
   }
 }
 
-console.log(newDict)
+console.log(newDict);
