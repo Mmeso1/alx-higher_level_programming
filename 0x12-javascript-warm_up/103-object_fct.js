2d48b76653f20356e08228eba3d4d2db651efa2a
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-14 05:06:23 -0800

    Last one yeahhhhh.....

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
-  value: 12,
-}
-console.log(myObject)
+  value: 12
+};
 
+console.log(myObject);
 myObject.incr = function () {
-  myObject.value += 1
-}
-myObject.incr()
-console.log(myObject)
-myObject.incr()
-console.log(myObject)
-myObject.incr()
-console.log(myObject)
+  myObject.value += 1;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
