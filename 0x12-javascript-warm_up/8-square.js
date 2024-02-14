35ff6b50cd87f6962bc919c5b93ecfa6c41b1839
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-14 04:30:03 -0800

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-const size = parseInt(process.argv[2])
+const size = parseInt(process.argv[2]);
 
 if (size) {
   for (let i = 0; i < size; ++i) {
-    let j = 0
+    let j = 0;
 
     for (; j < size; ++j) {
-      process.stdout.write('X')
+      process.stdout.write('X');
     }
 
     if (j === size) {
-      console.log('')
+      console.log('');
     }
   }
 } else {
-  console.log('Missing size')
+  console.log('Missing size');
 }
