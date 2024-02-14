03c89f44848db413c6c4df48fb052aaf42e64999
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-14 04:42:03 -0800

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
-function findSecondLargest(args) {
+function findSecondLargest (args) {
   if (args.length <= 1) {
-    console.log(0)
+    console.log(0);
   } else {
-    const integers = args.map(Number).sort((a, b) => b - a)
-    console.log(integers[1])
+    const integers = args.map(Number).sort((a, b) => b - a);
+    console.log(integers[1]);
   }
 }
 
-const args = process.argv.slice(2)
-findSecondLargest(args)
+const args = process.argv.slice(2);
+findSecondLargest(args);
