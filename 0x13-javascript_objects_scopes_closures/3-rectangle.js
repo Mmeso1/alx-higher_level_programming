29d2252204a50f9c6690ce10b714b12f8bf01c8a
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-02-14 03:34:10 +0100

    Update 3-rectangle.js -

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,21 +5,21 @@
 module.exports = class Rectangle {
   constructor(w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
-      this.width = w
-      this.height = h
+      this.width = w;
+      this.height = h;
     }
   }
 
   print() {
     for (let i = 0; i < this.height; i++) {
-      let j = 0
+      let j = 0;
       for (; j < this.width; j++) {
-        process.stdout.write('X')
+        process.stdout.write('X');
       }
 
       if (j == this.width) {
-        console.log('')
+        console.log('');
       }
     }
   }
-}
+};
