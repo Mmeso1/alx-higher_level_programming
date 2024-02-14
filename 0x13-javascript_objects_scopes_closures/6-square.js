70ec4bc555a284e44777def13f83d4e3fdcd96e3
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-02-14 03:48:20 +0100

    Update 6-square.js -

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // defines a square and inherits from Square of 5-square.js
 
-const OldSquare = require('./5-square')
+const OldSquare = require('./5-square');
 
 module.exports = class Square extends OldSquare {
-  constructor(size) {
-    super(size, size)
+  constructor (size) {
+    super(size, size);
   }
 
   charPrint(c = 'X') {
-    super.print(c)
+    super.print(c);
   }
-}
+};
