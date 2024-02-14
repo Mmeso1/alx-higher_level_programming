af2892db5456ca7a3e914f44ac2561f9817512a5
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-02-14 03:12:30 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js')
+const list = require('./100-data.js');
 
-console.log(list.list)
+console.log(list.list);
+
 const newList = list.list.map((index, val) => {
   return index * val
-})
-
-console.log(newList)
+});
+console.log(newList);
