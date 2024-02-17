6175a73d8ef319a48d9a75d00c69db1ca92a892c
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-17 05:31:58 -0800

    Meso's 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
+""" creates the State “California” with the City
+“San Francisco” from the database
 """
 
 from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    db = sessionmaker(bind=engine)()
 
-    session = Session()
-    cal_state = State(name='California')
-    sfr_city = City(name='San Francisco')
-    cal_state.cities.append(sfr_city)
+    cal_state = State(name="Carlifornia")
+    san_city = City(name="San Francisco")
+    cal_state.cities.append(san_city)
 
-    session.add(cal_state)
-    session.commit()
-    session.close()
+    db.add(cal_state)
+    db.commit()
+    db.close()
