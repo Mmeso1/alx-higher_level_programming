50c34e60d7940f456cce53449fc643e8f1cd5a1a
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-17 02:49:46 -0800

    Task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Start link class to table in database 
 """
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -11,12 +11,11 @@ if __name__ == "__main__":
             .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    Session() = sessionmaker(bind=engine)()
-    session = Session()
+    db = sessionmaker(bind=engine)()
 
     db.add(State(name="Louisiana"))
-    new_state_id = db.query(State.id).filter\
-            (State.name="Louisiana").first()
-    print(new_state_id)
-    session.commit()
-    session.close()
+    new_state = db.query(State).filter\
+            (State.name == "Louisiana").first()
+    print(new_state.id)
+    db.commit()
+    db.close()
