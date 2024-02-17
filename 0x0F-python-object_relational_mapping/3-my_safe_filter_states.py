f958393d9051415ada93fed97e54013395db705e
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-16 19:06:44 -0800

    Task 3 redo

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,6 +2,7 @@
 """
 This module takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
+zthis time we are making it SQL injection attack free.
 """
 import MySQLdb
 from sys import argv
@@ -12,8 +13,8 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
                              db=argv[3], charset='utf8')
         cur = db.cursor()
-        cur.execute("""SELECT * FROM states WHERE name = '{:s}' ORDER BY id"""
-                    .format(argv[4]))
+        cur.execute("""SELECT * FROM states WHERE name = %s ORDER BY id"""
+                    (argv[4]))
         states = cur.fetchall()
         for state in states:
             if state[1] == argv[4]:
