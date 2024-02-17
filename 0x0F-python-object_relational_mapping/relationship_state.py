f66200eacacc7342f2ab5508a7fd36690ea5d83b
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-17 04:57:59 -0800

    Task 100

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,9 +3,9 @@
 This module creates a python class model for
 the state table
 """
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -15,3 +15,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
