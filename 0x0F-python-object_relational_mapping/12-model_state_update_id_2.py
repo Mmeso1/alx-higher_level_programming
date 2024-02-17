24cac087e31de5c3fac2f297fc34b75a8c01ce29
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-17 03:01:13 -0800

    Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+""" changes the name of a State object from the database
 """
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    Session() = sessionmaker(bind=engine)()
-    session = Session()
+    session= sessionmaker(bind=engine)()
 
-    state = db.query(State).filter_by\
-            (State.id=2).first()
-    db.add(state.name="New Mexico")
+    state = session.query(State).filter_by\
+            (id=2).first()
+    session.add(State(name="New Mexico"))
     session.commit()
     session.close()
