a5d683b5e0f0f4c49b6ba078822cecae3ffc78c1
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2024-03-02 15:56:35 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" Please list 10 commits (from the most recent to oldest)
-of the repository “rails” by the user “rails”
 """
-
-import requests
-from sys import argv
-
+list 10 commits (from the most recent to oldest) of the repository and user
+sent in as arguments
+"""
 if __name__ == '__main__':
-    owner, repo = argv[1], argv[2]
-    res = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
-    data = res.json()
-
-    for commit in data[:10]:
-        res = f"{commit['sha']}: {commit['commit']['author']['name']}"
-        print(res)
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
