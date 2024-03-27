a64157ebd1618fb50f610629a2f3f92910dfb2c6
Mmeso1 <anisiubammesoma8@gmail.com>
2024-03-26 20:19:07 -0700

    Done Oh Done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,13 +8,13 @@ request(url, (error, response, body) => {
     return;
   }
   const data = JSON.parse(body);
-  data.characters.forEach((character)=>{
-	  request(character, (error, response, body) => {
-		  if (error) {
-			  return;
-		  }
-		  const actor = JSON.parse(body);
-		  console.log(actor.name);
-	  });
+  data.characters.forEach((character) => {
+    request(character, (error, response, body) => {
+      if (error) {
+        return;
+      }
+      const actor = JSON.parse(body);
+      console.log(actor.name);
+    });
   });
 });
