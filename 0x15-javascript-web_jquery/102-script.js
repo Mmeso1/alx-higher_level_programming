4bec2116ebcb6b42f2c28232b991916a5d824b48
Mmeso1 <anisiubammesoma8@gmail.com>
2024-04-04 03:57:00 -0700

    task 103 done

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,3 +1,4 @@
+//I am using this url instead cause the one provided is not getting any response and when i pass the provided url value to the browser, the url changes to this instead
 $(document).ready(function () {
 	$("#btn_translate").click(function () {
 		const lang = $("#language_code").val();
