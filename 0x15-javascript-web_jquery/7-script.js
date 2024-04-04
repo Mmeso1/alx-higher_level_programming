24d27fc293cc6ad73914608961192086e83d8b6b
Mmeso1 <anisiubammesoma8@gmail.com>
2024-04-03 22:46:59 -0700

    task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+$(function (){
+	$.ajax({
+		type: 'GET',
+		url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+		success: function(data) {
+			console.log(data);
+			$('div#character').text(data.name);
+		}
+	});
+});
